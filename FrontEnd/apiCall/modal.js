d74effc4c7ab38e60121b2a2cac01d29a27acc17
sarruto <122185097+sarruto@users.noreply.github.com>
2023-06-22 20:48:35 +0200

    css

--- FrontEnd/apiCall/modal.js
+++ FrontEnd/apiCall/modal.js
@@ -1,3 +1,6 @@
+
+
+
 // Fonction pour ouvrir la modale
 function openModal() {
     const modal = document.querySelector('.modal');
@@ -21,29 +24,77 @@ function openModal() {
   const closeButton = document.querySelector('.closeModal');
   closeButton.addEventListener('click', closeModal);
   
-  // Appel à l'API pour récupérer les travaux
-  async function fetchWorks() {
-    try {
-      const response = await fetch('http://localhost:5678/api/works');
-      const works = await response.json();
-      displayWorks(works);
-    } catch (error) {
-      console.error('Erreur lors de la récupération des travaux :', error);
-    }
-  }
-  
-  // Fonction pour afficher les travaux dans la modale
-  function displayWorks(works) {
-    const gallery = document.getElementById('gallery');
-    gallery.innerHTML = '';
-  
-    works.forEach(work => {
-      const image = document.createElement('img');
-      image.src = work.imageUrl;
-      image.alt = work.title;
-      gallery.appendChild(image);
-    });
-  }
+  displayWorks(works);
+const token = sessionStorage.getItem("token");
+if (token){
+  const openModalBtn = document.querySelector("#openModalBtn");
+  openModalBtn.classList.remove("hidden");
+}else{
+  displayBtn(categories, works);
+}
+
+
+
+
+// Faire appel à l'API pour les works
+
+const worksResponse = await fetch("http://localhost:5678/api/works");
+const works = await worksResponse.json();
+
+// Fonction pour afficher les works
+
+function displayWorks(works) {
+  const gallery = document.querySelector("#galleryPicure");
+  gallery.innerHTML = ""; // Effacer le contenu précédent de la galerie
+
+  works.forEach(work => {
+    const image = document.createElement("img");
+    image.src = work.imageUrl;
+    image.alt = work.title;
+
+    let figCaption = document.createElement("figcaption");
+    figCaption.textContent = work.title;
+
+    let figure = document.createElement("figure");
+
+    figure.appendChild(image);
+    figure.appendChild(figCaption);
+    gallery.appendChild(figure);
+  });
+}
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const buttonBox = document.querySelector(".buttonBox");
+buttonBox.classList.add("hidden");
+
+
+
+buttonBox.classList.remove("hidden");
+
+buttonBox.classList.toggle("hidden");
+*/
+
+
+
+
   
   // Appel initial pour récupérer les travaux et les afficher
   //fetchWorks();
