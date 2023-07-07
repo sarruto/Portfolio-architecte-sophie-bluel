b5a55c2ee0f44c8ae586940129620bd546664ead
sarruto <122185097+sarruto@users.noreply.github.com>
2023-07-08 00:58:24 +0200

    Supprimer un work

--- FrontEnd/apiCall/modal.js
+++ FrontEnd/apiCall/modal.js
@@ -1,126 +1,117 @@
 import { works } from "./works.js";
-// Fonction pour afficher les works dans la galerie
+
+// Fonction pour afficher les works dans la galerie de la modale
 function displayWorks(works) {
-  const gallery = document.querySelector('.gallery-modal');
-  gallery.innerHTML = ''; // Effacer le contenu précédent de la galerie
+  const gallery = document.querySelector(".gallery-modal");
+  gallery.innerHTML = ""; // Effacer le contenu précédent de la galerie
 
   works.forEach(work => {
-    const image = document.createElement('img');
+    const image = document.createElement("img");
     image.src = work.imageUrl;
     image.alt = work.title;
 
-    let editButton = document.createElement('button');
-    editButton.textContent = 'éditer';
-    editButton.classList.add('edit-button');
-    
-    let figCaption = document.createElement('figcaption');
-    figCaption.appendChild(editButton);
-    
+    let editButton = document.createElement("button");
+    editButton.textContent = "éditer";
+    editButton.classList.add("edit-button");
 
-    let figure = document.createElement('figure');
-    figure.dataset.id=work.id;
+    let figCaption = document.createElement("figcaption");
+    figCaption.appendChild(editButton);
+
+    let figure = document.createElement("figure");
+    figure.dataset.id = work.id;
 
     figure.appendChild(image);
     figure.appendChild(figCaption);
-    figure.appendChild(getIconTrash());
+    figure.appendChild(getIconTrash(work.id)); // Ajout de l'icône de suppression
+    figure.appendChild(getIconExpand);
     gallery.appendChild(figure);
   });
 }
-function getIconTrash() {
-  let img = document.createElement('img');
+
+function getIconTrash(workId) {
+  let img = document.createElement("img");
   img.src = "./assets/icons/trash.png";
-  let back = document.createElement('div');
-  back.classList.add('backicons');
-  img.classList.add('trashicons');
+  let back = document.createElement("div");
+  back.classList.add("backicons");
+  img.classList.add("trashicons");
   back.appendChild(img);
+
+  
+
+  // Ajout de l'événement de suppression du work
+  back.addEventListener("click", () => {
+    deleteWork(workId);
+    console.log("delete")
+  });
+
+  
+  
   return back;
 }
 
 
 
+// Fonction pour supprimer un work
+
+async function deleteWork(workId) {
+  try {
+    const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
+      method: "DELETE"
+    });
+
+    if (response.ok) {
+      // Suppression réussie, mettre à jour la galerie
+      const updatedWorksResponse = await fetch("http://localhost:5678/api/works");
+      const updatedWorks = await updatedWorksResponse.json();
+      displayWorks(updatedWorks);
+    } else {
+      console.error("Erreur lors de la suppression du work.");
+    }
+  } catch (error) {
+    console.error("Erreur lors de la suppression du work :", error);
+  }
+}
+
+
+function getIconExpand() {
+  let img = document.createElement('img');
+  img.src = "./assets/icons/expand.png";
+  let back = document.createElement('div');
+  back.classList.add('backiconsExpand');
+  img.classList.add('expandicons');
+  back.appendChild(img);
+
+
+
+
+}
+
 // Fonction pour ouvrir la modale et afficher la galerie des works
 function openModal() {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'block';
-  displayWorks(works)
-  //fetchWorks(); // Appel de la fonction pour récupérer et afficher les works dans la modale
+  const modal = document.getElementById("modal");
+  modal.style.display = "block";
+  displayWorks(works);
+
+  // Récupérer tous les éléments avec la classe "backicons" et ajouter l'événement de suppression
+  const backIcons = document.querySelectorAll(".backicons");
+  backIcons.forEach(icon => {
+    const workId = icon.parentElement.dataset.id; // Récupérer l'id du work
+    icon.addEventListener("click", () => {
+      deleteWork(workId);
+    });
+  });
 }
 
 // Fonction pour fermer la modale
 function closeModal() {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'none';
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
 }
 
-// Sélection du bouton d'ouverture de la modale
-const openModalBtn = document.getElementById('openModalBtn');
+// Événement pour ouvrir la modale lors du clic sur le bouton
+const openModalBtn = document.getElementById("openModalBtn");
+openModalBtn.addEventListener("click", openModal);
 
-// Sélection du bouton de fermeture de la modale
-const closeModalBtn = document.querySelector('.closeModal');
-
-// Écouteur d'événement pour le clic sur le bouton d'ouverture de la modale
-openModalBtn.addEventListener('click', openModal);
-
-// Écouteur d'événement pour le clic sur le bouton de fermeture de la modale
-closeModalBtn.addEventListener('click', closeModal);
-
-const modal=document.getElementById('modal');
-modal.addEventListener('click',closeModal) ;
-
-const modalContent=document.querySelector('.modal-content');
-modalContent.addEventListener('click',stopPropagation);
-function stopPropagation(event) {
-event.stopPropagation()
-
-}
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const buttonBox = document.querySelector(".buttonBox");
-buttonBox.classList.add("hidden");
-
-
-
-buttonBox.classList.remove("hidden");
-
-buttonBox.classList.toggle("hidden");
-*/
-
-
-
-
-  
-  // Appel initial pour récupérer les travaux et les afficher
-  //fetchWorks();
-  
-
-
-
-
-
-
-  /*
-  // Fonction pour récupérer les works depuis l'API
-async function fetchWorks() {
-  try {
-    const worksResponse = await fetch('http://localhost:5678/api/works');
-    const works = await worksResponse.json();
-    displayWorks(works);
-  } catch (error) {
-    console.log(error);
-  }
-}*/
+// Événement pour fermer la modale lors du clic sur la croix
+const closeModalBtn = document.querySelector(".closeModal");
+closeModalBtn.addEventListener("click", closeModal);
